fb8962b93eefc7620075f578750f7630edcd8b8f
JihwanB <leyqorwlghks@gmail.com>
2024-04-17 21:11:34 +0900

    랜딩페이지와 예약목록 페이지 복구를 위한 커밋

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
       <div class="banner-content">
         <h1>Welcome to ezCheck</h1>
         <p>Discover luxury stays with ezCheck</p>
-        <button type="button" class="btn btn-light">Book Now</button>
+        <router-link type="button" class="btn btn-light" to="/AvailableRoom">Book Now</router-link>
       </div>
     </div>
   </div>
@@ -28,7 +28,8 @@ export default {
 }
 
 .banner {
-  background-image: url('https://www.conradseoul.co.kr/revolution/content/fileImage.do?fileId=11671&cntntsSn=11669'); /* 원하는 배경 이미지 URL로 변경 */
+  background-image: url('https://www.conradseoul.co.kr/revolution/content/fileImage.do?fileId=11671&cntntsSn=11669');
+  /* 원하는 배경 이미지 URL로 변경 */
   background-size: cover;
   background-position: center;
   height: 100vh;
