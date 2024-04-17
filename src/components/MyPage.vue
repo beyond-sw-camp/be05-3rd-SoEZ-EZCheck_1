756f6247a25ecbc583ee6c5487cd07e5433976be
JihwanB <leyqorwlghks@gmail.com>
2024-04-17 15:21:27 +0900

    Added user info retrieval and user account delete feature

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -4,13 +4,15 @@
     <h1>My Page</h1>
     <div class="navigation">
       <!-- 네비게이션 링크, 선택적으로 router-link 사용 가능 -->
-      <button @click="getUserInfo = 'info'">내 정보</button>
+      <button type="button" @click="selectedTab = 'info'">내 정보</button>
       <button @click="selectedTab = 'reservations'">내 예약 목록</button>
     </div>
 
+    <hr />
     <!-- 선택된 탭에 따라 다른 컴포넌트를 표시 -->
     <div v-if="selectedTab === 'info'">
-      <p>내 정보 컴포넌트가 여기에 표시됩니다.</p>
+      <!-- '내 정보' 버튼을 눌렀을 때 UserInfo 컴포넌트를 렌더링 -->
+      <UserInfo :userInfo="userInfo" @update:userInfo="userInfo = $event" />
     </div>
     <div v-if="selectedTab === 'reservations'">
       <MyReservations />
@@ -18,34 +20,41 @@
   </div>
 </template>
 
-
-
-
 <script>
+import { ref, onMounted } from 'vue';
 import Header from './Header.vue';
 import MyReservations from './MyReservations.vue';
+import UserInfo from '@/components/UserInfo.vue';
+
 export default {
+
   name: 'MyPage',
   components: {
     Header,
-    MyReservations
+    MyReservations,
+    UserInfo,
   },
-  data() {
-    return {
-      selectedTab: 'reservations'  // 기본적으로 예약 목록 탭을 선택
-    };
-  },
-  mounted() {
-    // 예약 데이터를 불러오는 로직, API 호출 등을 여기에 추가
-    this.fetchReservations();
-  },
-  methods: {
-    fetchReservations() {
+
+  setup() {
+
+    const userInfo = ref({})
+    const selectedTab = ref('reservations');  // 기본적으로 예약 목록 탭을 선택
+
+    const fetchReservations = () => {
       // API 호출을 통해 예약 데이터 불러오기
-      // 예시: this.reservations = [{ id: 1, description: '호텔 예약', date: '2024-04-20' }];
+      // 예시: state.reservations = [{ id: 1, description: '호텔 예약', date: '2024-04-20' }];
       // 실제 API 호출로 대체해야 함
     }
+
+    onMounted(fetchReservations);
+
+    return {
+      userInfo,
+      selectedTab,
+    }
+
   }
+
 }
 </script>
 
